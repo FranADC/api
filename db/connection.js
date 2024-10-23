fc88db74d6055b2a270421e4bdef28bc917c9ad8
FranADC <franacunadacosta@gmail.com>
2024-10-24 00:37:03 +0200

    Conexión a base de datos y primera consulta

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mariadb = require("mariadb");
+
+//esto no es una conexión, aqui guardamos todos los datos de conexión
+const pool = mariadb.createPool({
+  host: "localhost",
+  user: "admin",
+  password: "daw2pass",
+  database: "dramones_y_mazmorras",
+  port: "33006",
+});
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-//Guardamos a mano el paquete express
+//Guardamos a mano el paquete express, el que nos permite use node como api
 const express = require("express");
-
 //Creamos una instancia con la que trabajar
 const api = express();
 
+//Donde juardamos los datos de la conexión a la base de datos
+const dbConnection = require("./db/connection");
+
+//Permite hacer peticiones desde otras url o dominios
+const cors = require("cors");
+//
+const corsOptions = {
+  origin: "http://localhost:5173", //origen que permitimos(front-end)
+  methods: "GET,POST,PUT,DELETE", //las acciones que permitimos
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+api.use(cors(corsOptions));
+
 //Prueba de api ("ruta",función(peticion(entrada de datos),respuesta(salida de datos)))
 api.get("/", (request, response) => {
-  response.send("flashbang");
+  response.send("Hola mundo");
 });
 
+//async marca que una funcion tiene await, await detiene el programa hasta obtener lo establecido
+api.get("/db", async (req, res) => {
+  let connect;
+  try {
+    connect = await dbConnection.getConnection();
+    const fila = await connect.query("SELECT * FROM Magia");
+    res.json(fila);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error al obtener los usuarios");
+  } finally {
+    if (connect) connect.end();
+  }
+});
+
+//Puerto donde escucha node, siempre va al final.
 api.listen(3000, () => {
-  console.log("Puerto 3000 listo para quemar inocentes");
+  console.log("Puerto 3000 inicializado");
 });
