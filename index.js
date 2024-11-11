4f9c816008166d6c9e7df9bdfd6b19e5e293b5ad
FranADC <franacunadacosta@gmail.com>
2024-11-11 20:38:26 +0100

    Primera peticon select All y por id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 //Creamos una instancia con la que trabajar
 const api = express();
 
-//Donde juardamos los datos de la conexión a la base de datos
+//Donde guardamos los datos de la conexión a la base de datos
 const dbConnection = require("./db/connection");
 
 //Permite hacer peticiones desde otras url o dominios
@@ -14,7 +14,6 @@ const corsOptions = {
   methods: "GET,POST,PUT,DELETE", //las acciones que permitimos
   allowedHeaders: "Content-Type,Authorization",
 };
-
 api.use(cors(corsOptions));
 
 //Prueba de api ("ruta",función(peticion(entrada de datos),respuesta(salida de datos)))
@@ -23,11 +22,31 @@ api.get("/", (request, response) => {
 });
 
 //async marca que una funcion tiene await, await detiene el programa hasta obtener lo establecido
-api.get("/db", async (req, res) => {
+api.get("/conjuros", async (req, res) => {
   let connect;
   try {
     connect = await dbConnection.getConnection();
-    const fila = await connect.query("SELECT * FROM Magia");
+    const fila = await connect.query(
+      "SELECT c.ID_conjuro, c.Nombre_conjuro, c.Nivel_conjuro, c.RangoArea, c.Somatico, c.Verbal, c.Material, c.Duracion, c.Concentracion, c.Ritual, c.Imagen_conjuro, c.DescCorta, em.Nombre_escuela, tl.Nombre_lanzamiento, al.Nombre_Alcance  FROM dramones_y_mazmorras.Conjuros c  JOIN dramones_y_mazmorras.EscuelasMagia em  on c.Escuela_Magia = em.ID_clase JOIN dramones_y_mazmorras.TiemposLanzamiento tl  on c.TiempoLanz  = tl.ID_Lanzamiento JOIN dramones_y_mazmorras.AlcanceLanzamiento al  on c.Alcance  = al.ID_Alcance ;"
+    );
+    res.json(fila);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error al obtener los usuarios");
+  } finally {
+    if (connect) connect.end();
+  }
+});
+
+api.get("/conjuro/:ID", async (req, res) => {
+  const ID = req.params.ID;
+  let connect;
+  try {
+    connect = await dbConnection.getConnection();
+    const fila = await connect.query(
+      "SELECT c.ID_conjuro, c.Nombre_conjuro, c.Nivel_conjuro, c.RangoArea, c.Somatico, c.Verbal, c.Material, c.Duracion, c.Concentracion, c.Ritual, c.Imagen_conjuro, c.DescCorta, em.Nombre_escuela, tl.Nombre_lanzamiento, al.Nombre_Alcance  FROM dramones_y_mazmorras.Conjuros c  JOIN dramones_y_mazmorras.EscuelasMagia em  on c.Escuela_Magia = em.ID_clase JOIN dramones_y_mazmorras.TiemposLanzamiento tl  on c.TiempoLanz  = tl.ID_Lanzamiento JOIN dramones_y_mazmorras.AlcanceLanzamiento al  on c.Alcance  = al.ID_Alcance WHERE ID_conjuro = ?;",
+      [ID]
+    );
     res.json(fila);
   } catch (err) {
     console.log(err);
